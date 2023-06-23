81c613749a83df05027428b107e9d023b211685e
Simon Atiegar <atiegardelali@gmail.com>
2023-06-23 16:51:36 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,3 @@
 // Mediconnect app, a health care app for patients and doctors to keep patients records
 // Pharmacy can also use this app to keep records patients and prescriptions 
-import logo from './logo.svg';
-import './App.css';
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Mediconnect</h1>
-      </header>
-    </div>
-  );
-
-}
-
-export default App;

a58c9d767d11cfde7d9f86374adfef69c283f4f1
Simon Atiegar <atiegardelali@gmail.com>
2023-06-23 16:51:24 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
+// Mediconnect app, a health care app for patients and doctors to keep patients records
+// Pharmacy can also use this app to keep records patients and prescriptions 
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Mediconnect</h1>
       </header>
     </div>
   );
+
 }
 
 export default App;
